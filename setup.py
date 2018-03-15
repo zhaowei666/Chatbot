9f223d578571f073b5ab30b887e710a0c8104be1
Zhaowei Ren <rzwdsg@gmail.com>
2018-03-14 23:47:51 -0400

    made structure like a library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='chatbot',
+    version='1.0.0',
+    author='Zhaowei',
+    author_email='rzwdsg@gmail.com',
+    url='https://github.com/zhaowei666/Chatbot',
+    installed_requires=[]
+)
